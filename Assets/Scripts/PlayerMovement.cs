d4279ac47a53fd1db90cd896d50c7f3e40f936b3
Denyous <denizolce76@gmail.com>
2025-10-09 16:56:02 +0300

    PlayerMovement Update
    -PlayerMovement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 direction;
+
+    [SerializeField] public float gravity = -9.8f;
+    [SerializeField] public float jumpMultip = 15f;
+   
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+          
+            direction = Vector3.up * jumpMultip;
+
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                direction = Vector3.up * jumpMultip;
+            }
+
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+    }
+}

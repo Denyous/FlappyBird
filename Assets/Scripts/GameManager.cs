2dfbe6dcf769cc3482eb1094745b5214fd3fce1e
Denyous <denizolce76@gmail.com>
2025-10-13 22:50:54 +0300

    UI Setup and Func

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    private static int score = 0;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] GameObject gameoverScreen;
+    [SerializeField] PlayerMovement player;
+    private int score = 0;
 
-    public static void IncreaseScore()
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        PauseGame();
+
+    }
+    public void PlayGame()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+        gameoverScreen.SetActive(false);
+
+        player.enabled = true;
+
+        PipesMove[] pipes = FindObjectsOfType<PipesMove>();
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+
+        Time.timeScale = 1;
+
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
+
+    public void IncreaseScore()
     {
         score++;
-        Debug.Log("Score Increased!!");
+        scoreText.text = score.ToString();
+
     }
 
-    public static void GameOver()
+    public void GameOver()
     {
-        Debug.Log("You Died");
+
+        PauseGame();
+        gameoverScreen?.SetActive(true);
+
     }
+
 
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,8 +8,15 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] public float gravity = -9.8f;
     [SerializeField] public float jumpMultip = 15f;
-   
-    
+
+    private void OnEnable()
+    {
+        Vector3 position = transform.position;
+        position.y = 0f;
+        transform.position = position;
+        direction = Vector3.zero;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
@@ -38,12 +45,12 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
-            GameManager.GameOver();
+            FindAnyObjectByType<GameManager>().GameOver();
         }
 
         if (collision.gameObject.CompareTag("Score"))
         {
-            GameManager.IncreaseScore();
+            FindAnyObjectByType<GameManager>().IncreaseScore();
         }
     }
 }
